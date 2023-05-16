1f25f230c3fd7cb336feb295e82c4de1dcc035fe
yungtonym <yunggtonym@gmail.com>
2023-05-16 19:10:51 +0700

    Make the bot work infinitely

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ bot = telebot.TeleBot('6292095375:AAGQ9S9FtYEomvSbX6XNjgjuakBY9XM9j_s')
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Hello!')
+
+
+bot.polling(none_stop=True)
