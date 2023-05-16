ccbfb513b843b4846becd2fc7a801ac7e878dc13
yungtonym <yunggtonym@gmail.com>
2023-05-16 19:05:42 +0700

    Add start message handler

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import telebot
 bot = telebot.TeleBot('6292095375:AAGQ9S9FtYEomvSbX6XNjgjuakBY9XM9j_s')
 
 
+@bot.message_handlers(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Hello!')
