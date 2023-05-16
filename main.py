57721b3b527a75b2b0017f259027ce7e64bfc452
yungtonym <yunggtonym@gmail.com>
2023-05-16 19:08:19 +0700

    Fix message handler

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import telebot
 bot = telebot.TeleBot('6292095375:AAGQ9S9FtYEomvSbX6XNjgjuakBY9XM9j_s')
 
 
-@bot.message_handlers(commands=['start'])
+@bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'Hello!')
