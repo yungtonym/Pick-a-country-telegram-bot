aaac82839ae7b3ffaaf41aaac9b61584dcdacc34
yungtonym <yunggtonym@gmail.com>
2023-05-16 19:24:27 +0700

    Add new start commands

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot
 bot = telebot.TeleBot('6292095375:AAGQ9S9FtYEomvSbX6XNjgjuakBY9XM9j_s')
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'main', 'hello'])
 def main(message):
     bot.send_message(message.chat.id, 'Hello!')
 
