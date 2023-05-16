1710f22cf3df3874346cd4a72f2d4de1e952b495
yungtonym <yunggtonym@gmail.com>
2023-05-16 20:13:35 +0700

    Add handler for flag info

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import webbrowser
 
 bot = telebot.TeleBot('6292095375:AAGQ9S9FtYEomvSbX6XNjgjuakBY9XM9j_s')
 
@@ -6,6 +7,11 @@ bot = telebot.TeleBot('6292095375:AAGQ9S9FtYEomvSbX6XNjgjuakBY9XM9j_s')
 @bot.message_handler(commands=['start', 'main', 'hello'])
 def main(message):
     bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}!')
+
+
+@bot.message_handler(commands=['your-flag'])
+def flag_info(message):
+    webbrowser.open('https://www.flagofplanetearth.com/')
 
 
 @bot.message_handler(commands=['help'])
